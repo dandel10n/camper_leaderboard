569e05d627ba7755acacca91769fa6617428ff62
Mariya Radchuk <radchuk.mary@gmail.com>
2017-05-31 20:27:37 +0100

    Convert tads to spaces

--- src/index.js
+++ src/index.js
@@ -10,50 +10,50 @@ import './index.css';
 4.   рефреш - обновление данных для текущей страницы
 */
 class Leaderboard extends React.Component {
-	constructor() {
-    	super();
-    	this.state = {
-			JSON: [],
-			sort: 'recent'
-    	}
-    	this.updateJSON = this.updateJSON.bind(this);
+    constructor() {
+        super();
+        this.state = {
+            JSON: [],
+            sort: 'recent'
+        }
+        this.updateJSON = this.updateJSON.bind(this);
 
-		this.data = {
-			recent: [],
-			alltime: []
-		}
-	}
+        this.data = {
+            recent: [],
+            alltime: []
+        }
+    }
 
-	links = {
-		recent: "https://fcctop100.herokuapp.com/api/fccusers/top/recent",
-		alltime: "https://fcctop100.herokuapp.com/api/fccusers/top/alltime"
-	}
+    links = {
+        recent: "https://fcctop100.herokuapp.com/api/fccusers/top/recent",
+        alltime: "https://fcctop100.herokuapp.com/api/fccusers/top/alltime"
+    }
 
     updateJSON(period) {
         var href = this.links[period];
 
         if (this.data[period].length > 0) {
-			this.setState({
-				JSON: this.data[period],
-				sort: period
-			});
+            this.setState({
+                JSON: this.data[period],
+                sort: period
+            });
         } else {
-	        fetch(href).then( (response) => {
-				return response.json();
-			}).then( (data) => {
-				this.setState({
-					JSON: data,
-					sort: period
-				});
-				this.data[period] = data;
-			}).catch(function(ex) {
-				console.log('parsing failed', ex)
-			});
-		}
+            fetch(href).then( (response) => {
+                return response.json();
+            }).then( (data) => {
+                this.setState({
+                    JSON: data,
+                    sort: period
+                });
+                this.data[period] = data;
+            }).catch(function(ex) {
+                console.log('parsing failed', ex)
+            });
+        }
     }
 
     componentDidMount() {
-		this.updateJSON("recent");
+        this.updateJSON("recent");
     }
 
     render() {
@@ -64,53 +64,53 @@ class Leaderboard extends React.Component {
 }
 
 const Table = (props) => {
-	let rows = props.currentState.JSON.map((person, indexOfPerson) => {
-		return <PersonRow
-			key = {person.username}
-			data = {person}
-			number = {indexOfPerson + 1}
-			/>
+    let rows = props.currentState.JSON.map((person, indexOfPerson) => {
+        return <PersonRow
+            key = {person.username}
+            data = {person}
+            number = {indexOfPerson + 1}
+            />
     })
-	return (
-		<div className="container">
-			<header>Camper Leaderboard</header>
-			<table>
-				<thead>
-					<tr>
-						<th>№</th>
-						<th>Camper Name</th>
-						<th><a href="#" className={props.currentState.sort === 'recent' ? 'link active' : 'link'} onClick={() => {props.updateJSON('recent')} }>Points in past 30 days</a></th>
-						<th><a href="#" className={props.currentState.sort === 'alltime' ? 'link active' : 'link'} onClick={() => {props.updateJSON('alltime')} }>All time points</a></th>
-					</tr>
-				</thead>
-				<tbody>
-					{rows}
-				</tbody>
-			</table>
-		</div>
-	)
+    return (
+        <div className="container">
+            <header>Camper Leaderboard</header>
+            <table>
+                <thead>
+                    <tr>
+                        <th>№</th>
+                        <th>Camper Name</th>
+                        <th><a href="#" className={props.currentState.sort === 'recent' ? 'link active' : 'link'} onClick={() => {props.updateJSON('recent')} }>Points in past 30 days</a></th>
+                        <th><a href="#" className={props.currentState.sort === 'alltime' ? 'link active' : 'link'} onClick={() => {props.updateJSON('alltime')} }>All time points</a></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 const PersonRow = (props) => {
   return (
-	<tr>
-		<td className="userNumber">
-			{props.number}
-		</td>
-		<td className="username">
-			<a href={"https://www.freecodecamp.com/" + props.data.username}><img src={props.data.img} alt="user avatar"/>{props.data.username}</a>
-		</td>
-		<td className="userdata">
-			{ props.data.recent }
-		</td>
-		<td className="userdata">
-			{ props.data.alltime }
-		</td>
+    <tr>
+        <td className="userNumber">
+            {props.number}
+        </td>
+        <td className="username">
+            <a href={"https://www.freecodecamp.com/" + props.data.username}><img src={props.data.img} alt="user avatar"/>{props.data.username}</a>
+        </td>
+        <td className="userdata">
+            { props.data.recent }
+        </td>
+        <td className="userdata">
+            { props.data.alltime }
+        </td>
     </tr>
   );
 }
 
 ReactDOM.render(
-  	<Leaderboard />,
-  	document.getElementById('root')
+      <Leaderboard />,
+      document.getElementById('root')
 );
